5b7d6945178de115be914f2d2580a56b1e1c188b
Wireless4024 <wireless4024@proton.me>
2022-08-23 20:59:57 +0700

    add file via upload

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import Router from "svelte-spa-router"
+	import wrap   from "svelte-spa-router/wrap"
+
+	const routes = {
+		'/': wrap({
+			asyncComponent: () => import("./lib/Home.svelte")
+		})
+	}
+</script>
+
+<main>
+	<Router {routes}/>
+</main>
