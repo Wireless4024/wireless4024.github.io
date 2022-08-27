c6a5ce2aa196af06721890456f6ae80941de45fc
wireless4024 <wireless4024@proton.me>
2022-08-27 07:16:37 +0700

    misc: Optimize performance?

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,12 @@
+<svelte:head>
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Ubuntu:300,400,500,600,700">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Ubuntu+Mono">
+</svelte:head>
 <script lang="ts">
 	import Router from "svelte-spa-router"
-	import AppBar from "./lib/AppBar.svelte";
-	import routes from "./util/routes.js";
+	import AppBar from "./lib/AppBar.svelte"
+	import routes from "./util/routes.js"
 </script>
 <AppBar>
 	<main>
