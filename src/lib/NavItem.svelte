5d4adc5d4c7fb3710ba0627dd37fd2c011a62e22
Wireless4024 <wireless4024@proton.me>
2022-08-29 09:13:05 +0700

    allow nav item to active if href ends with s

--- src/lib/NavItem.svelte
+++ src/lib/NavItem.svelte
@@ -11,8 +11,14 @@
 
 	export let icon: string
 	export let href: string
+
+	let activated
+	$:{
+		const loc = $location
+		activated = href.endsWith('s') ? loc.startsWith(href.substring(0, href.length - 1)) : href === loc
+	}
 </script>
-<Item href="javascript:void(0)" on:click={()=>push(href)} activated={href===$location}>
+<Item href="javascript:void(0)" on:click={()=>push(href)} {activated}>
 	{#if icon}
 		<Graphic class="material-icons" aria-hidden="true">{icon}</Graphic>
 	{/if}
