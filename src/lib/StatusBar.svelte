7032b358e0dd2c97e0e93c8e7dd55af378fba620
Wireless4024 <wireless4024@proton.me>
2022-10-04 17:52:30 +0700

    fix: Reload component bug

--- src/lib/StatusBar.svelte
+++ src/lib/StatusBar.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
 	import type {KitchenComponentDev} from '@smui/snackbar/kitchen';
 	import Kitchen                    from "@smui/snackbar/kitchen";
-	import {onMount}                  from "svelte"
+	import {
+		onDestroy,
+		onMount
+	}                                 from "svelte"
 	import {status_list}              from "../util/base"
 
 	let kitchen: KitchenComponentDev;
 
 	function push_status(status: string) {
-		console.log(status)
 		if (status)
 			kitchen.push({
 				props        : {
@@ -18,9 +20,10 @@
 			});
 	}
 
+	let unsub: () => void
 	onMount(function () {
 		let consumed = false
-		status_list.subscribe(it => {
+		unsub = status_list.subscribe(it => {
 			if (consumed) {
 				consumed = false
 				return
@@ -30,6 +33,10 @@
 			push_status(elem)
 			status_list.set(it)
 		})
+	})
+
+	onDestroy(function () {
+		unsub && unsub()
 	})
 </script>
 <Kitchen bind:this={kitchen} dismiss$class="material-icons"></Kitchen>
