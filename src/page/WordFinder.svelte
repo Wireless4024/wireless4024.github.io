4cd15d5768bf0809ecd9e2a1b93a9e899b3aa36a
Wireless4024 <wireless4024@proton.me>
2022-10-15 16:52:22 +0700

    chore: More feature to word finder

--- src/page/WordFinder.svelte
+++ src/page/WordFinder.svelte
@@ -14,6 +14,15 @@
 
 	async function load_text(file: File) {
 		words = (await file.text()).split("\n")
+		compute_max_len()
+	}
+
+	async function load_http(url: string) {
+		words = (await fetch(url).then(it => it.text())).split("\n")
+		compute_max_len()
+	}
+
+	function compute_max_len() {
 		let _max_len = 0
 		for (let word of words) {
 			_max_len = Math.max(word.length, _max_len)
@@ -25,19 +34,38 @@
 	let max_len: number = 15
 	let max_match: number = 50
 	let filter: string = ""
-	let filter_type: 'includes' | 'startsWith' | 'endsWith' = 'includes'
+	let filter_type: 'includes' | 'startsWith' | 'endsWith' | 'regex' = 'includes'
 
 	function _filter(filter: string, ...any) {
 		if (filter?.length > 0) {
+			const ftype = filter_type
 			let res: string[] = []
 			let _max_word_len = 0
-			for (let word of words) {
-				if (word.length < max_len && word?.[filter_type]?.(filter)) {
-					res.push(word)
-					_max_word_len = Math.max(word.length, _max_word_len)
+			if (ftype == "regex") {
+				let regex: RegExp
+				try {
+					regex = new RegExp(filter)
+				} catch (e) {
+					return
 				}
-				if (res.length > max_match) {
-					break
+				for (let word of words) {
+					if (word.length <= max_len && regex.test(word)) {
+						res.push(word)
+						_max_word_len = Math.max(word.length, _max_word_len)
+					}
+					if (res.length > max_match) {
+						break
+					}
+				}
+			} else {
+				for (let word of words) {
+					if (word.length <= max_len && word?.[ftype]?.(filter)) {
+						res.push(word)
+						_max_word_len = Math.max(word.length, _max_word_len)
+					}
+					if (res.length > max_match) {
+						break
+					}
 				}
 			}
 			filtered_words = res
@@ -58,23 +86,36 @@
 	<div>
 		Find
 		<Textfield bind:value={filter} label="Filter"></Textfield>
-		<Select bind:value={filter_type} label="Select Menu">
+		<Select bind:value={filter_type} label="Filter type">
 			<Option value="includes">includes</Option>
 			<Option value="startsWith">startsWith</Option>
 			<Option value="endsWith">endsWith</Option>
+			<Option value="regex">regex</Option>
 		</Select>
 	</div>
 	<FormField align="end" style="display: flex;">
 		<Slider style="flex-grow: 1;" bind:value={max_len} min="3" max={max_word_len} step="1"/>
-		<span slot="label" style="padding-right: 12px; width: max-content; display: block;">Max word length ({max_len})</span>
+		<span slot="label" style="padding-right: 12px; width: max-content; display: block;">Max word length ({max_len}
+			)</span>
 	</FormField>
 	<FormField align="end" style="display: flex;">
 		<Slider style="flex-grow: 1;" bind:value={max_match} min="50" max="5000" step="50"/>
-		<span slot="label" style="padding-right: 12px; width: max-content; display: block;">Max result ({max_match})</span>
+		<span slot="label" style="padding-right: 12px; width: max-content; display: block;">Max result ({max_match}
+			)</span>
 	</FormField>
 	<div>
 		<Textfield bind:files={selected_file} label="" type="file"/>
-		(word per line)
+		{#if words.length}
+			{words.length} words
+		{:else}
+			(word per line)
+		{/if}
+	</div>
+	<div>
+		<Button on:click={()=>load_http("https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt")}>
+			Load from
+			github.com/dwyl/english-words
+		</Button>
 		<Button href="https://github.com/dwyl/english-words" target="_blank">finding word list?</Button>
 	</div>
 	{#if filter}
